962a821d663f8f777db535f0a96a41c12d8e5ea3
Michal Wrona <wronamichal01@gmail.com>
2021-05-30 16:18:08 +0200

    Update settings.py for herokuapp

--- website/settings.py
+++ website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#&$ns(dc#o$xtr#2fow_xj90)$3i9u1!*ubii4#rdo0b)=)it)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://claser.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
