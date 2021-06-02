34e3940ec682f1ff42393b77e006b8a3d17a1c9d
Michal Wrona <wronamichal01@gmail.com>
2021-06-02 13:21:21 +0200

    Delete all files from Lesson Plan

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     path('classroom/<str:classroom_pk>/subject/<str:subject_pk>/posts', views.dashboard_posts, name="dashboard_posts"),
     path('classroom/<str:classroom_pk>/subject/<str:subject_pk>/homeworks', views.dashboard_homeworks, name="dashboard_homeworks"),
     path('classroom/<str:classroom_pk>/subject/<str:subject_pk>/marks', views.dashboard_marks, name="dashboard_marks"),
-    path('classroom/<str:classroom_pk>/subject/<str:subject_pk>/lesson_plan', views.dashboard_lesson_plan, name="dashboard_lesson_plan"),
-
 ]
