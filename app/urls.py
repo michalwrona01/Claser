6a26c08af3d1ec0d442a7f9fb5924c528d94d814
Michal Wrona <wronamichal01@gmail.com>
2021-06-04 15:25:15 +0200

    Added delete posts in student panel of subject

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('subject_panel/<str:pk>', views.subject_panel, name="subject_panel"),
+    path('subject_panel/<str:subject_pk>', views.subject_panel, name="subject_panel"),
+    path('subject_panel/<str:subject_pk>/posts/<str:post_id>/delete', views.subject_panel_post_delete, name="subject_panel_post_delete"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect, render
 from .models import *
 from django.contrib.auth.decorators import login_required
 from .decorators import allowed_user, redirect_to_home_page_due_to_role, redirect_to_choice_profile
 from dashboard.forms import PostCreationForm
+from django.contrib import messages
 
 
 
@@ -30,9 +32,9 @@ def home(request):
 
 @login_required(login_url='login')
 @allowed_user(allowed_roles=['student'])
-def subject_panel(request, pk):
+def subject_panel(request, subject_pk):
     classroom = request.user.student.classroom
-    subject = classroom.subjects.filter(id=pk).first()
+    subject = classroom.subjects.filter(id=subject_pk).first()
     posts = Post.objects.filter(classroom=classroom).filter(subject=subject)
     homeworks = Homework.objects.filter(subject=subject)
 
@@ -44,10 +46,10 @@ def subject_panel(request, pk):
     form_post = PostCreationForm(initial=initial_values)
 
     if request.method == "POST":
+
         form_post = PostCreationForm(request.POST, initial=initial_values)
         if form_post.is_valid():
             form_post.save()
-
 
     context = {'posts' : posts,
                 'subject' : subject,
@@ -56,8 +58,28 @@ def subject_panel(request, pk):
                 'form_post' : form_post,
                 }
 
-
     return render(request, 'app/subject_panel.html', context)
+
+@login_required(login_url='login')
+@allowed_user(allowed_roles=['student'])
+def subject_panel_post_delete(request, subject_pk, post_id):
+    try:
+        post_obj = Post.objects.get(id=post_id)
+        if post_obj.created_person == request.user:
+            post_obj.delete()
+            messages.success(request, "You just delete post!")
+        else:
+            messages.error(request, "You can't delete this post!")
+    except ObjectDoesNotExist:
+        messages.error(request, "You doesn't have any posts or want to delete an existing post!")      
+    finally:
+        return redirect('subject_panel', subject_pk)
+
+
+    
+
+
+    
 
 
 
